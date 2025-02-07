bef4b5e40ccc310a73206fc24da676a2d395bfa1
tofuramen <tofaialrahman@gmail.com>
2025-02-06 23:19:26 -0500

    added some front-end to the application

--- src/main/java/com/bookclub/controller/RegistrationController.java
+++ src/main/java/com/bookclub/controller/RegistrationController.java
@@ -1,21 +1,27 @@
 package com.bookclub.controller;
 
+import com.bookclub.model.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegistrationController {
 
-    //this loads the html page
+    //this loads the html page and also adds a new User object to my model
     @RequestMapping("/register")
-    public String signUp() {
+    public String signUp(Model model) {
+        model.addAttribute("user", new User());
         return "register";
     }
 
-    @PostMapping("/register/confirmation")
-    public String confirmation() {
-        return "confirmation";
+    @PostMapping("/register")
+    public String confirmation(@Valid @ModelAttribute("user") User user, BindingResult result) {
+        return "redirect:/confirmation";
     }
 
 
